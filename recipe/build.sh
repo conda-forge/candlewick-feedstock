c9587d1d1327db13a289939c4644e0779c4a0d45
Yann de Mont-Marin <ydemontm@wifi-pro-82-134.paris.inria.fr>
2025-04-18 11:42:07 +0200

    Fix crosscompilation using numpy

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,32 @@
 # Required by apple SDK symbol check: https://conda-forge.org/docs/maintainer/knowledge_base/#newer-c-features-with-old-sdk
 CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
+export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+echo $BUILD_NUMPY_INCLUDE_DIRS
+echo $TARGET_NUMPY_INCLUDE_DIRS
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+  export Python3_NumPy_INCLUDE_DIR=$TARGET_NUMPY_INCLUDE_DIRS
+else
+  export Python3_NumPy_INCLUDE_DIR=$BUILD_NUMPY_INCLUDE_DIRS
+fi
+
+
 cmake -S . -B build ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=$PREFIX \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_PINOCCHIO_VISUALIZER=ON \
   -DBUILD_PYTHON_BINDINGS=ON \
   -DBUILD_EXAMPLES=OFF \
-  -DBUILD_TESTING=OFF
+  -DBUILD_TESTING=OFF \
+  -DPython3_NumPy_INCLUDE_DIR=$Python3_NumPy_INCLUDE_DIR \
+  -DPYTHON_EXECUTABLE=$PYTHON
+
 
 cmake --build build --config Release -j${CPU_COUNT}
 
